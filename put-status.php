23ba75bc085c78c1c06e171e2cc0257c2e9a1f65
Flavia Paganelli <flavia@looselytied.com>
2012-01-23 10:17:11 +0100

    last flush operation is actually in miliseconds, not microseconds

--- put-status.php
+++ put-status.php
@@ -289,14 +289,14 @@ function add_host_metrics( $cw, $ismaster, $server_status, $lag) {
 			'Dimensions' => $replset,
 			'Value' => $server_status['backgroundFlushing']['last_ms'],
 			'Timestamp' => $timestamp,
-			'Unit' => 'Microseconds'
+			'Unit' => 'Miliseconds'
 		),
 		array(
 			'MetricName' => 'LastFlushOperation',
 			'Dimensions' => $dimensions,
 			'Value' => $server_status['backgroundFlushing']['last_ms'],
 			'Timestamp' => $timestamp,
-			'Unit' => 'Microseconds'
+			'Unit' => 'Miliseconds'
 		),
 		array(
 			'MetricName' => 'OpenCursors',
